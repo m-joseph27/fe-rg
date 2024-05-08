601e62793e28805001e3e8f5b211e0662c87d7dd
Muhammad Yusuf <muhammadyusuf@Air-Muhammad.lan>
2024-05-08 22:09:20 +0700

    fixing list card order

--- src/components/pages/list/index.js
+++ src/components/pages/list/index.js
@@ -1,6 +1,8 @@
 import './index.scss';
 import ListCard from '../../molecules/list-card/list-card';
 import FilterBar from '../../molecules/filter_bar/filter-bar';
+import Label from '../../atoms/label/label';
+import SortBar from '../../molecules/sort_bar/sort_bar';
 
 const PageList = () => {
   return (
@@ -9,7 +11,25 @@ const PageList = () => {
         <FilterBar />
       </div>
       <div className="card-section">
-        <ListCard />
+        <div className="header">
+          <div>
+            <Label value={'Product List'} />
+          </div>
+          <div className="sorter">
+            <span>Urutkan</span>
+            <div>
+              <SortBar />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="list-of-card">
+          <ListCard />
+          <ListCard />
+          <ListCard />
+          <ListCard />
+          <ListCard />
+        </div>
       </div>
     </div>
   );
