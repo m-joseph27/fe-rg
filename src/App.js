cd7d9dc08992e5e542d50b945478e8720aa1204c
Muhammad Yusuf <muhammadyusuf@Air-Muhammad.lan>
2024-05-09 12:05:37 +0700

    fishing card when it hover

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <NavigationBar />
       <PageList />
-      {/* <PageDetailList /> */}
+      <PageDetailList />
       {/* <FilterBar /> */}
       {/* <SortBar /> */}
       {/* <ListCard /> */}
--- src/components/molecules/list-card/list-card.js
+++ src/components/molecules/list-card/list-card.js
@@ -6,11 +6,13 @@ import Point from '../../../assets/point.svg';
 import { Rating } from 'react-simple-star-rating';
 import FavButton from "../../atoms/button/fav-button";
 import UnFavButton from "../../atoms/button/unfav-button";
+import Button from "../../atoms/button/button";
 
 const ListCard = () => {
   const [ altImage, setAltImage ] = useState('unloved');
-  const [ stock, setStock ] = useState(6);
+  const [ stock, setStock ] = useState(5);
   const [ soldOut, setSoldOut ] = useState(false);
+  const [ isHovered, setIsHovered ] = useState(false);
 
   const onClickBtnFav = () => {
     setAltImage('unloved')
@@ -19,10 +21,73 @@ const ListCard = () => {
   const onClickBtnUnFav = () => {
     setAltImage('loved');
   }
+  
+  const onMouseEnter = () => {
+    if (!soldOut) {
+      console.log('here');
+      setIsHovered(true);
+    }
+  }
+
+  const onMouseLeave = () => {
+    if (!soldOut) {
+      console.log('leave');
+      setIsHovered(false);
+    }
+  }
 
   return (
     <>
-      <div className="list-card">
+      <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className={isHovered ? "hovered-list-card" : "list-card"}>
+      {
+        isHovered &&
+        <div>
+          <div className='hovered-item-ready'>
+            <div className="hovered-stock">
+              {
+                soldOut ?
+                <span className="item-sold-out">Sold Out</span>
+                :
+                <>
+                  {
+                    stock > 5 ? <span className="in-stock">In Stock</span> : <span className="under-stock">{`Stock < 5`}</span>
+                  }
+                </>
+              }
+            </div>
+            <div className="hovered-img-phone">
+              {
+                soldOut ?
+                <img className={soldOut ? 'img-sold' : ''} src={Phone} alt="phone" />
+                :
+                <img src={Phone} alt="phone" />
+              }
+            </div>
+            <div className="hovered-phone-desc">
+              <div className="phone-name">
+                <span>Samsung Galaxy S9 -Midnight Black 4/64 GB</span>
+              </div>
+              <div className="btn-detail">
+                <Button type={'secondary'} text={'View detail'} />
+              </div>
+              <div className="btn-favorite">
+                {
+                  altImage === 'loved' ?
+                  <button disabled={soldOut} onClick={onClickBtnFav}>
+                    <FavButton />
+                  </button>
+                  :
+                  <button disabled={soldOut} onClick={onClickBtnUnFav}>
+                    <UnFavButton />
+                  </button>
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+      {
+        !isHovered &&
         <div className={soldOut ? 'sold-out' : 'item-ready' }>
           <div className="tag-image">
             {
@@ -80,6 +145,7 @@ const ListCard = () => {
             </div>
           </div>
         </div>
+      }
       </div>
     </>
   );
