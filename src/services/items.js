181db637ae5d2621fd628cfa15f9589e8877aee6
Muhammad Yusuf <muhammadyusuf@Air-Muhammad.lan>
2024-05-09 16:06:51 +0700

    working on adding app service

---
+++ src/services/items.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const API_BASE_URL = `https://recruitment.dev.rollingglory.com/api/v2/gifts?page[number]=1&page[size]=6`;
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+const apiClient = axios.create({
+  baseURL: 'https://recruitment.dev.rollingglory.com/api/v2/gifts?page[number]=1&page[size]=6',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+export const GetItems = async (endpoint) => {
+  try {
+    const response = await apiClient.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+const GetDetailItem = async (endpoint) => {
+  try {
+    const response = await api.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+export default apiClient;
