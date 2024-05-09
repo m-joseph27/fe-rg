59a6a9cc9b82d3c98b13c266137a74b8ca8e853e
Muhammad Yusuf <muhammadyusuf@Air-Muhammad.lan>
2024-05-09 12:27:07 +0700

    add icon for detail button

--- src/components/atoms/button/button.js
+++ src/components/atoms/button/button.js
@@ -4,6 +4,9 @@ const Button = (props) => {
   return (
     <div className="button">
       <button className={`btn-${props.type}`}>
+        {props.icon &&
+          <i class="fa-regular fa-eye"></i>
+        }
         { props.text }
       </button>
     </div>
