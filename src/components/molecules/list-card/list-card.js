14c4ddf2332944b8d49f6d9304dd5ace98392e4e
Muhammad Yusuf <muhammadyusuf@Air-Muhammad.lan>
2024-05-09 12:31:01 +0700

    fix card border style

--- src/components/molecules/list-card/list-card.js
+++ src/components/molecules/list-card/list-card.js
@@ -68,7 +68,7 @@ const ListCard = () => {
                 <span>Samsung Galaxy S9 -Midnight Black 4/64 GB</span>
               </div>
               <div className="btn-detail">
-                <Button type={'detail'} text={'View detail'} />
+                <Button type={'detail'} text={'View detail'} icon={true} />
               </div>
               <div className="btn-favorite">
                 {
